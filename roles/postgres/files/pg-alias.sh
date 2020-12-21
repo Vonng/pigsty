1dfe984757334a29997c4a138389afd2fa7bc4f4
Vonng <fengruohang@outlook.com>
2020-12-21 21:04:34 +0800

    rewrite pg alias

--- roles/postgres/files/pg-alias.sh
+++ roles/postgres/files/pg-alias.sh
@@ -1,42 +1,69 @@
-# for testing purpose
+##########################################################
+#                         admin                          #
+##########################################################
+# - postgres - #
+alias pg-up='sudo systemctl restart postgres'
+alias pg-dw='sudo systemctl stop postgres'
+alias pg-st='systemctl status postgres'
+alias pg-reload='sudo systemctl reload postgres'
+alias pg-restart='sudo systemctl restart postgres'
+alias pg-stop='pg_ctl -D /pg/data stop'
+alias pg-start='pg_ctl -D /pg/data start'
+alias pg-tlog='tail -f /pg/data/log/*.csv'
+alias pg-log='vi /pg/data/log/postgresql-$(date +%a).csv'
 
-alias synctime="sudo ntpdate -u time.pool.aliyun.com"
-alias viconf="vi /pg/data/postgresql.conf"
-alias vihba="vi /pg/data/pg_hba.conf"
-alias vilog='tail -f /pg/data/log/postgresql-$(date +%a).csv'
-alias cdlog='cd /pg/data/log'
-alias cddata='cd /pg/data'
+alias pg-d="cd /pg/data"
+alias pg-l="cd /pg/data/log"
+alias pg-c="vi /pg/data/postgresql.conf"
+alias pg-h="vi /pg/data/pg_hba.conf"
 
-alias pr="psql -qAtc \"SELECT CASE pg_is_in_recovery() WHEN TRUE THEN 'standby' ELSE 'primary' END\";"
-alias pgrepl='psql -Axc "TABLE pg_stat_replication;"'
-alias pgrecv='psql -Axc "TABLE pg_stat_replication;"'
-alias psa="ps aux | grep "
-alias pst='ps aux | grep postgres'
-alias pst='ps aux | grep postgres'
-
-alias nexp="curl -sL localhost:9100/metrics | grep -v '#' | grep node_"
-alias pexp="curl -sL localhost:9630/metrics | grep -v '#' | grep pg_"
-alias pgup='sudo systemctl restart postgres'
-alias pgdw='sudo systemctl stop postgres'
-alias pgst='systemctl status postgres'
-alias pgreload='sudo systemctl reload postgres'
-alias pgrestart='sudo systemctl restart postgres'
-alias stoppg='pg_ctl -D /pg/data stop'
-alias startpg='pg_ctl -D /pg/data start'
-
-alias ptup='sudo systemctl start patroni'
-alias ptdw='sudo systemctl stop  patroni'
-alias ptst='systemctl status patroni'
-alias ptlog='tail -f /pg/log/patroni.log'
+# - pgbouncer - #
 alias pgb='psql -p6432 -dpgbouncer'
-alias pgbup='sudo systemctl restart pgbouncer'
-alias pgbdw='sudo systemctl stop pgbouncer'
-alias pgbrestart='systemctl restart pgbouncer'
-alias pgbst='sudo systemctl status pgbouncer'
-alias pgbstat='psql -p6432 -dpgbouncer -xc "SHOW STATS;"'
-alias pgbpool='psql -p6432 -dpgbouncer -xc "SHOW POOLS;"'
-alias pgbreload='/usr/bin/pgbouncer -d -R /etc/pgbouncer/pgbouncer.ini'
-alias pglog='tail -f /pg/data/log/*.csv'
-alias plog='tail -f /pg/log/*.log'
-alias pg2md=" sed 's/+/|/g' | sed 's/^/|/' | sed 's/$/|/' |  grep -v rows | grep -v '||'"
+alias pgb-st='systemctl status pgbouncer'
+alias pgb-ps='ps aux | grep pgbouncer'
+alias pgb-up='sudo systemctl restart pgbouncer'
+alias pgb-dw='sudo systemctl stop pgbouncer'
+alias pgb-restart='systemctl restart pgbouncer'
+alias pgb-reload='systemctl reload pgbouncer'
+alias pgb-new='/usr/bin/pgbouncer -d -R /etc/pgbouncer/pgbouncer.ini'
+alias pgb-stat='psql -p6432 -dpgbouncer -xc "SHOW STATS;"'
+alias pgb-pool='psql -p6432 -dpgbouncer -xc "SHOW POOLS;"'
+
+alias pgb-dir="cd /etc/pgbouncer"
+alias pgb-conf="cat /etc/pgbouncer/pgbouncer.ini"
+alias pgb-hba="cat /etc/pgbouncer/pgb_hba.conf"
+alias pgb-user="cat /etc/pgbouncer/database.txt"
+alias pgb-db="cat /etc/pgbouncer/userlist.txt"
+
+# - patroni - #
+alias pt-up='sudo systemctl start patroni'
+alias pt-dw='sudo systemctl stop  patroni'
+alias pt-st='systemctl status patroni'
+alias pt-ps='ps aux | grep patroni'
+alias pt-log='tail -f /pg/log/patroni.log'
+alias pt-tlog='tail -f /pg/log/*.log'
+
+
+##########################################################
+# pgsql info
+##########################################################
+alias pg-db="oid2name | grep -v postgres | grep -v template"
+alias pg-r="psql -qAXtwc \"SELECT CASE pg_is_in_recovery() WHEN TRUE THEN 'replica' ELSE 'primary' END\";"
+alias pg-repl='psql -qAXtwc "TABLE pg_stat_replication;"'
+alias pg-recv='psql -qAXtwc "TABLE pg_stat_wal_receiver;"'
+alias pg-md=" sed 's/+/|/g' | sed 's/^/|/' | sed 's/$/|/' |  grep -v rows | grep -v '||'"
+
+##########################################################
+# exporter info
+##########################################################
+alias node-mt="curl -sL localhost:9100/metrics | grep -v '#' | grep node_"
+alias pg-mt="curl -sL localhost:9630/metrics | grep -v '#' | grep pg_"
+alias pg-mt="curl -sL localhost:9631/metrics | grep -v '#' | grep pg_"
+
+##########################################################
+# misc
+##########################################################
+alias synctime="sudo ntpdate -u time.pool.aliyun.com"
+alias psa="ps aux | grep "
 alias wal=walarchiver
+
