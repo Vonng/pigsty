6ec8c244708ff098d2c60ba8d5b29cb13e8b51fb
Vonng <fengruohang@outlook.com>
2020-12-22 16:16:08 +0800

    fix consul prune logic * now consul_data_dir is used for cleanse

--- roles/consul/files/stop_consul.sh
+++ roles/consul/files/stop_consul.sh
@@ -18,11 +18,5 @@ if ps -u consul -o command | grep -q 'consul agent' ; then
 	ps -u consul -o pid:1,command | grep 'consul agent' | awk '{print $1}' | xargs kill -9
 fi
 
-# remove consul config dir and data dir
-rm -rf /var/lib/consul /etc/consul.d
-mkdir /var/lib/consul /etc/consul.d
-chown consul /var/lib/consul /etc/consul.d
-chmod 0775 /var/lib/consul /etc/consul.d
-
 # guaranteed success
 exit 0
