b54e052c1714d4aa028cf9a594a2047e902e5c69
alemacci <maccioni.alessio@gmail.com>
2022-06-06 16:41:14 +0200

    added scram-sha-256 support

--- roles/postgres/templates/pg-user.sql
+++ roles/postgres/templates/pg-user.sql
@@ -97,7 +97,7 @@ GRANT "{{ role }}" TO "{{ user.name }}";
 
 {% if 'pgbouncer' in user and user.pgbouncer|bool == true %}
 -- User '{{ user.name }}' will be added to /etc/pgbouncer/userlist.txt via
--- /pg/bin/pgbouncer-create-user '{{ user.name }}' '{% if 'password' in user %}{{ user.password }}{% else %}auto{% endif %}'
+-- /pg/bin/pgbouncer-create-user '{{ user.name }}' 'AUTO'
 {% else %}
 -- User '{{ user.name }}' will NOT be added to /etc/pgbouncer/userlist.txt
 {% endif %}
