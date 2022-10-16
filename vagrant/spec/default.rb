53342ad50c7405683d1e6108b06e258062d784fd
Vonng <rh@vonng.com>
2022-10-16 14:45:02 +0800

    refactor vagrant vm spec modules * now using vagrant/switch to switch IaaS Provisioning templates

---
+++ vagrant/spec/default.rb
@@ -0,0 +1,7 @@
+# pigsty default singleton vm (3C6G)
+Specs = [
+  {"name" => "meta",   "ip" => "10.10.10.10", "cpu" => "3",  "mem" => "6128", "image" => "generic/centos7" },
+  #{"name" => "node-1", "ip" => "10.10.10.11", "cpu" => "1",  "mem" => "2048", "image" => "generic/centos7" },
+  #{"name" => "node-2", "ip" => "10.10.10.12", "cpu" => "1",  "mem" => "2048", "image" => "generic/centos7" },
+  #{"name" => "node-3", "ip" => "10.10.10.13", "cpu" => "1",  "mem" => "2048", "image" => "generic/centos7" },
+]
