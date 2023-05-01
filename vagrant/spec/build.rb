a7c4a1bfdfbc463b289546a987d7b1cd26d902a8
Vonng <rh@vonng.com>
2023-05-01 22:17:17 +0800

    add more packages to build template

--- vagrant/spec/build.rb
+++ vagrant/spec/build.rb
@@ -1,8 +1,8 @@
 # pigsty 3 nodes building environment: centos7 / rocky8 / rocky9
 Specs = [
-  {"name" => "meta",   "ip" => "10.10.10.10", "cpu" => "2",  "mem" => "4096", "image" => "generic/centos7" },
-  {"name" => "node-1", "ip" => "10.10.10.11", "cpu" => "2",  "mem" => "4096", "image" => "generic/rocky8"  },
-  {"name" => "node-2", "ip" => "10.10.10.12", "cpu" => "2",  "mem" => "4096", "image" => "generic/rocky9"  },
+  {"name" => "meta",   "ip" => "10.10.10.10", "cpu" => "2",  "mem" => "8192", "image" => "generic/centos7" },
+  {"name" => "node-1", "ip" => "10.10.10.11", "cpu" => "2",  "mem" => "8192", "image" => "generic/rocky8"  },
+  {"name" => "node-2", "ip" => "10.10.10.12", "cpu" => "2",  "mem" => "8192", "image" => "generic/rocky9"  },
 ]
 
 # Alternative building env: rhel7 / rhel8 / rhel9
