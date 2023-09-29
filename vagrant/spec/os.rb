fcee2e530b107a4e74afcabd820bb83cbf4b7458
Vonng <rh@vonng.com>
2023-09-29 12:59:00 +0800

    add os.yml and os.rb for vagrant sandbox for ubuntu/debian

---
+++ vagrant/spec/os.rb
@@ -0,0 +1,20 @@
+# pigsty os checking environment for rhe compatible distributinos & ubuntu & debian
+
+Specs = [
+
+  { "name" => "centos7"  ,"ip" => "10.10.10.7"    , "cpu" => "4"    , "mem" => "8182"    , "image" =>  "generic/centos7"     },
+  { "name" => "rocky8"   ,"ip" => "10.10.10.8"    , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/rocky8"      },
+  { "name" => "rocky9"   ,"ip" => "10.10.10.9"    , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/rocky9"      },
+  { "name" => "debian11" ,"ip" => "10.10.10.11"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/debian11"    },
+  { "name" => "debian12" ,"ip" => "10.10.10.12"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/debian12"    },
+  { "name" => "ubuntu20" ,"ip" => "10.10.10.20"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2004"  },
+  { "name" => "ubuntu22" ,"ip" => "10.10.10.22"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2204"  },
+  { "name" => "rhel7"    ,"ip" => "10.10.10.27"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/rhel7"       },
+  { "name" => "rhel8"    ,"ip" => "10.10.10.28"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/rhel8"       },
+  { "name" => "rhel9"    ,"ip" => "10.10.10.29"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/rhel9"       },
+  { "name" => "alma8"    ,"ip" => "10.10.10.38"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/alma8"       },
+  { "name" => "alma9"    ,"ip" => "10.10.10.39"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/alma9"       },
+  { "name" => "oracle8"  ,"ip" => "10.10.10.48"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/oracle8"     },
+  { "name" => "oracle9"  ,"ip" => "10.10.10.49"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/oracle9"     },
+
+]
