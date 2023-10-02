bbe547bd137d1022f2f9cd599a67aade174592b9
Vonng <rh@vonng.com>
2023-10-02 10:22:26 +0800

    add ubuntu vagrant specs

---
+++ vagrant/spec/ubuntu.rb
@@ -0,0 +1,12 @@
+# pigsty os checking environment for rhe compatible distributinos & ubuntu & debian
+
+Specs = [
+
+  { "name" => "debian11" ,"ip" => "10.10.10.11"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/debian11"    },
+  { "name" => "debian12" ,"ip" => "10.10.10.12"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/debian12"    },
+  { "name" => "ubuntu20" ,"ip" => "10.10.10.20"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2004"  },
+  { "name" => "ubuntu22" ,"ip" => "10.10.10.22"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2204"  },
+  { "name" => "ubuntu2"  ,"ip" => "10.10.10.23"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2204"  },
+  { "name" => "ubuntu3"  ,"ip" => "10.10.10.24"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2204"  },
+
+]
