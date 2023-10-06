471ca21ab850a19d08a7d81b0825e6e7deb7f917
Vonng <rh@vonng.com>
2023-10-06 16:27:47 +0800

    add meta to ubuntu template, use build-elx in os template

--- vagrant/spec/ubuntu.rb
+++ vagrant/spec/ubuntu.rb
@@ -2,6 +2,7 @@
 
 Specs = [
 
+  { "name" => "meta"     ,"ip" => "10.10.10.10"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2204"  },
   { "name" => "debian11" ,"ip" => "10.10.10.11"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/debian11"    },
   { "name" => "debian12" ,"ip" => "10.10.10.12"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/debian12"    },
   { "name" => "ubuntu20" ,"ip" => "10.10.10.20"   , "cpu" => "4"    , "mem" => "8192"    , "image" =>  "generic/ubuntu2004"  },
