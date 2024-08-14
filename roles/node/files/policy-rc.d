77c21173ae129f4cbeaf5d24ba5f75c0a818963a
Vonng <rh@vonng.com>
2024-08-14 20:51:46 +0800

    disable postgresql-common package auto configure

--- roles/node/files/policy-rc.d
+++ roles/node/files/policy-rc.d
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # List of service names that should not be started by deb install/update
-STOP_SERVICES="nginx loki dnsmasq haproxy keepalived docker promtail minio etcd postgresql pgbouncer patroni redis-server"
+STOP_SERVICES="nginx loki dnsmasq haproxy keepalived docker promtail minio etcd postgresql pgbouncer patroni redis-server postgresql-common postgresql-16 postgresql-15 postgresql-14 postgresql-13 postgresql-12 postgresql-11 postgresql-10"
 
 # Check if the service is in the STOP_SERVICES list
 for SERVICE in $STOP_SERVICES; do
