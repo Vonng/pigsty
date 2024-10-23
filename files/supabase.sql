2d052293b137c5b5d176e101de2eafa8f7e27c64
Vonng <rh@vonng.com>
2024-10-23 19:01:32 +0800

    update supabase.yml config templates

--- files/supabase.sql
+++ files/supabase.sql
@@ -500,10 +500,26 @@ GRANT ALL ON ALL ROUTINES IN SCHEMA realtime TO postgres, dashboard_user;
 -- migrate:up
 
 -- update owner for auth.uid, auth.role and auth.email functions
+DO $$
+BEGIN
     ALTER FUNCTION auth.uid owner to supabase_auth_admin;
-ALTER FUNCTION auth.role owner to supabase_auth_admin;
-ALTER FUNCTION auth.email owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.uid to supabase_auth_admin';
+END $$;
 
+DO $$
+BEGIN
+    ALTER FUNCTION auth.role owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.role to supabase_auth_admin';
+END $$;
+
+DO $$
+BEGIN
+    ALTER FUNCTION auth.email owner to supabase_auth_admin;
+EXCEPTION WHEN OTHERS THEN
+    RAISE WARNING 'Error encountered when changing owner of auth.email to supabase_auth_admin';
+END $$;
 -- migrate:down
 
 
