cea19ada5b8a83ef2e8789fef3d9cd5b8dd18961
Vonng <rh@vonng.com>
2024-11-17 20:14:49 +0800

    add minio vagrant spec

---
+++ vagrant/spec/minio.rb
@@ -0,0 +1,10 @@
+# full: pigsty full-featured 4-node sandbox for HA-testing & tutorial & practices
+
+Specs = [
+
+  { "name" => "minio-0" , "ip" => "10.10.10.10" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky8"  },
+  { "name" => "minio-1" , "ip" => "10.10.10.11" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky8"  },
+  { "name" => "minio-2" , "ip" => "10.10.10.12" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky8"  },
+  { "name" => "minio-3" , "ip" => "10.10.10.13" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky8"  },
+
+]
