d672fbb7b1a0abd6fff93e9a03769c58f32fc73e
Vonng <rh@vonng.com>
2024-11-18 11:11:30 +0800

    allow overwrite minio_volumes to expand cluster

--- vagrant/spec/minio.rb
+++ vagrant/spec/minio.rb
@@ -2,9 +2,15 @@
 
 Specs = [
 
-  { "name" => "minio-0" , "ip" => "10.10.10.10" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
-  { "name" => "minio-1" , "ip" => "10.10.10.11" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
-  { "name" => "minio-2" , "ip" => "10.10.10.12" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
-  { "name" => "minio-3" , "ip" => "10.10.10.13" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+
+  { "name" => "minio-1" , "ip" => "10.10.10.10" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+  { "name" => "minio-2" , "ip" => "10.10.10.11" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+  { "name" => "minio-3" , "ip" => "10.10.10.12" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+  { "name" => "minio-4" , "ip" => "10.10.10.13" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+
+  #{ "name" => "minio-5" , "ip" => "10.10.10.14" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+  #{ "name" => "minio-6" , "ip" => "10.10.10.15" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+  #{ "name" => "minio-7" , "ip" => "10.10.10.16" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
+  #{ "name" => "minio-8" , "ip" => "10.10.10.17" ,  "cpu" => "1" ,  "mem" => "2048" ,  "image" => "generic/rocky9"  },
 
 ]
